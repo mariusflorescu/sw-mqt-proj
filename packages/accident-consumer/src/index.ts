2bb0095980c8461bcb0072f7cfadb18ad940fc72
Marius Florescu <mariusflorescu23@icloud.com>
2023-11-09 14:38:41 +0200

    chore: validate message

--- packages/accident-consumer/src/index.ts
+++ packages/accident-consumer/src/index.ts
@@ -1,10 +1,23 @@
 import { kafkaConsumer } from "./lib/kafka";
+import { z } from "zod";
+
+const messageSchema = z.object({
+  street: z.string(),
+  timestamp: z.number(),
+});
 
 const run = async () => {
   await kafkaConsumer.connect();
   await kafkaConsumer.subscribe({ topic: "accident", fromBeginning: true });
   await kafkaConsumer.run({
-    eachMessage: async ({ message }) => {
+    eachMessage: async ({ message: bufMsg }) => {
+      if (!bufMsg || !bufMsg.value) return;
+
+      const rawMessage = JSON.parse(bufMsg.value.toString());
+      const validatedMessage = messageSchema.safeParse(rawMessage);
+      if (!validatedMessage.success) return;
+      const message = validatedMessage.data;
+
       console.log(message);
     },
   });
